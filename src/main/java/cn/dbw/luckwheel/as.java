97dedbb3770fdc32a8b1948ed79e273452f0a57e
gtt19960308 <980234538@qq.com>
2019-07-04 08:41:55 +0800

    price

--- src/main/java/cn/dbw/luckwheel/as.java
+++ src/main/java/cn/dbw/luckwheel/as.java
@@ -1,5 +1,7 @@
 package cn.dbw.luckwheel;
 
+import java.math.BigDecimal;
+
 /**
  * @ClassName as
  * @Description TODO
@@ -8,4 +10,5 @@ package cn.dbw.luckwheel;
  * @Version 1.0
  **/
 public class as {
+    private BigDecimal price;
 }
